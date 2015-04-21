d6c89d41ddca52258bb7c0dcecd7279bc90aa04e
Steve-Jones <steve@stevenjon.es>
2015-04-21 10:17:58 -0400

    Initial Commit

---
+++ ColoringGraph/src/coloringGraph.java
@@ -0,0 +1,17 @@
+/*
+ * Input: An undirected graph G<V,E>
+ * Output: Is G 2 colored
+ * 			Yes: Valid coloring - For all (u,v)inE color(u) != color(v)
+ * http://ideone.com/3crr4y
+ */
+
+
+
+
+public class coloringGraph {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
